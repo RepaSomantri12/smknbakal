6fe1f059f334cde970c948797c6cf0cb00b0e122
RepaSomantri12 <repasomantri414@gmail.com>
2024-10-21 18:33:53 +0700

    contact.php

--- main/contact.php
+++ main/contact.php
@@ -1,81 +1,80 @@
-
 <section id="contact" class="contact section">
 
-<!-- Section Title -->
-<div class="container section-title" data-aos="fade-up">
-  <h2>Contact</h2>
-  <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
-</div><!-- End Section Title -->
+  <!-- Section Title -->
+  <div class="container section-title" data-aos="fade-up">
+    <h2>Get in Touch</h2>
+    <p>We would love to hear from you! Whether you have a question, feedback, or just want to say hello, feel free to reach out.</p>
+  </div><!-- End Section Title -->
 
-<div class="container" data-aos="fade-up" data-aos-delay="100">
+  <div class="container" data-aos="fade-up" data-aos-delay="100">
 
-  <div class="row gy-4">
+    <div class="row gy-4">
 
-    <div class="col-lg-6">
-      <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="200">
-        <i class="bi bi-geo-alt"></i>
-        <h3>Address</h3>
-        <p>A108 Adam Street, New York, NY 535022</p>
-      </div>
-    </div><!-- End Info Item -->
-
-    <div class="col-lg-3 col-md-6">
-      <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="300">
-        <i class="bi bi-telephone"></i>
-        <h3>Call Us</h3>
-        <p>+1 5589 55488 55</p>
-      </div>
-    </div><!-- End Info Item -->
-
-    <div class="col-lg-3 col-md-6">
-      <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="400">
-        <i class="bi bi-envelope"></i>
-        <h3>Email Us</h3>
-        <p>info@example.com</p>
-      </div>
-    </div><!-- End Info Item -->
-
-  </div>
-
-  <div class="row gy-4 mt-1">
-    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus" frameborder="0" style="border:0; width: 100%; height: 400px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div><!-- End Google Maps -->
-
-    <div class="col-lg-6">
-      <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="400">
-        <div class="row gy-4">
-
-          <div class="col-md-6">
-            <input type="text" name="name" class="form-control" placeholder="Your Name" required="">
-          </div>
-
-          <div class="col-md-6 ">
-            <input type="email" class="form-control" name="email" placeholder="Your Email" required="">
-          </div>
-
-          <div class="col-md-12">
-            <input type="text" class="form-control" name="subject" placeholder="Subject" required="">
-          </div>
-
-          <div class="col-md-12">
-            <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
-          </div>
-
-          <div class="col-md-12 text-center">
-            <div class="loading">Loading</div>
-            <div class="error-message"></div>
-            <div class="sent-message">Your message has been sent. Thank you!</div>
-
-            <button type="submit">Send Message</button>
-          </div>
-
+      <div class="col-lg-6">
+        <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="200">
+          <i class="bi bi-geo-alt"></i>
+          <h3>Alamat</h3>
+          <p>94H6+CC Hegarwangi, Kabupaten Tasikmalaya, Jawa Barat</p>
         </div>
-      </form>
-    </div><!-- End Contact Form -->
+      </div><!-- End Info Item -->
+
+      <div class="col-lg-3 col-md-6">
+        <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="300">
+          <i class="bi bi-telephone"></i>
+          <h3>No Hp</h3>
+          <p>+(0265) 581377</p>
+        </div>
+      </div><!-- End Info Item -->
+
+      <div class="col-lg-3 col-md-6">
+        <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="400">
+          <i class="bi bi-envelope"></i>
+          <h3>Email </h3>
+          <p>@smknegeribantarkalong.com</p>
+        </div> 
+      </div><!-- End Info Item -->
+
+    </div>
+
+    <div class="row gy-4 mt-1">
+      <div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
+        <iframe src="https://www.google.com/maps/place/SMK+Negeri+Bantarkalong/@-7.6214198,108.100807,14z/data=!4m6!3m5!1s0x2e65e3bb34a35c21:0x57a71906404ba203!8m2!3d-7.6214198!4d108.1111067!16s%2Fg%2F1hm54101l?hl=id&entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D" frameborder="0" style="border:0; width: 100%; height: 400px;" allowfullscreen="" loading="lazy"></iframe>
+      </div><!-- End Google Maps -->
+
+      <div class="col-lg-6">
+        <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="400">
+          <div class="row gy-4">
+
+            <div class="col-md-6">
+              <input type="text" name="name" class="form-control" placeholder="Your Name" required>
+            </div>
+
+            <div class="col-md-6">
+              <input type="email" class="form-control" name="email" placeholder="Your Email" required>
+            </div>
+
+            <div class="col-md-12">
+              <input type="text" class="form-control" name="subject" placeholder="Subject" required>
+            </div>
+
+            <div class="col-md-12">
+              <textarea class="form-control" name="message" rows="6" placeholder="Your Message" required></textarea>
+            </div>
+
+            <div class="col-md-12 text-center">
+              <div class="loading">Loading</div>
+              <div class="error-message"></div>
+              <div class="sent-message">Your message has been sent. Thank you!</div>
+
+              <button type="submit" class="btn btn-primary">Send Message</button>
+            </div>
+
+          </div>
+        </form>
+      </div><!-- End Contact Form -->
+
+    </div>
 
   </div>
-
-</div>
 
 </section>
