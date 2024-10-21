dd3fe857562ed7ff4c885527f48487d219285193
ndamasfat <nndamasfat@gmail.com>
2024-10-21 14:54:59 +0700

    panggil file hero

--- main/hero.php
+++ main/hero.php
@@ -8,7 +8,7 @@
           <h1 data-aos="fade-up">Welcome to <span>SMKBANTARKALONG </span></h1>
           <p data-aos="fade-up" data-aos-delay="100">Bergabunglah dengan SMKN Bantarkalong dan temukan berbagai program keahlian yang siap mempersiapkan kamu untuk dunia kerja!<br></p>
           <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
-            <a href="#about" class="btn-get-started">Get Started</a>
+            <a href="#about" class="btn-get-started">LOGIN</a>
             <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
           </div>
           <img src="assets/img/hero-services-img.webp" class="img-fluid hero-img" alt="" data-aos="zoom-out" data-aos-delay="300">
