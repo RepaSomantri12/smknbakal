c5e3e94ed9758aa55a57e79bb0db412abc9e2e36
ndamasfat <nndamasfat@gmail.com>
2024-10-22 10:38:38 +0700

    panggil file hero

--- main/hero.php
+++ main/hero.php
@@ -5,8 +5,9 @@
       </div>
       <div class="container text-center">
         <div class="d-flex flex-column justify-content-center align-items-center">
-          <h1 data-aos="fade-up">Welcome to <span>SMKBANTARKALONG </span></h1>
-          <p data-aos="fade-up" data-aos-delay="100">Bergabunglah dengan SMKN Bantarkalong dan temukan berbagai program keahlian yang siap mempersiapkan kamu untuk dunia kerja!<br></p>
+        <h1 data-aos="fade-up"> Welcome to <span style="color: red;">SMKBANTARKALONG</span> </h1>
+<p data-aos="fade-up" data-aos-delay="100">Bergabunglah dengan SMKN Bantarkalong dan temukan berbagai program keahlian yang siap mempersiapkan kamu untuk dunia kerja!<br></p>
+
           <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
             <a href="#about" class="btn-get-started">LOGIN</a>
             <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
