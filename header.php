47193ebc36fac6d99581f77f1650d1b9c82d5a19
RepaSomantri12 <repasomantri414@gmail.com>
2024-10-24 14:31:10 +0700

    panggil main

--- header.php
+++ header.php
@@ -50,9 +50,9 @@
         <ul>
           <li><a href="index.php?page=home" class="active">Home</a></li>
           <li><a href="index.php?page=about">About</a></li>
-          <li><a href="index.php?page=jurusan">Jurusan</a></li>
+          <li><a href="index.php?page=jurusan">JURUSAN</a></li>
           <li><a href="index.php?page=fasilitas">Fasilitas</a></li>
-          <li><a href="index.php?page=contact">Contact</a></li>
+          <li><a href="index.php?page=contact">KONTAK</a></li>
           <l1><a href="index.php?page=kegiatan">Kegiatan</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
--- main/jurusan.php
+++ main/jurusan.php
@@ -2,7 +2,7 @@
 
   <!-- Section Title -->
   <div class="container section-title" data-aos="fade-up">
-    <h2>Jurusan di SMKN Bantarkalong</h2>
+    <h2>Jurusan di SMKN BANTARKALONG</h2>
     <p>Kenali berbagai jurusan yang ditawarkan di SMKN Bantarkalong untuk masa depan yang lebih baik.</p>
   </div><!-- End Section Title -->
 
@@ -10,16 +10,13 @@
     <div class="row justify-content-between">
 
       <div class="col-lg-5 d-flex align-items-center">
-
         <ul class="nav nav-tabs" data-aos="fade-up" data-aos-delay="100">
           <li class="nav-item">
             <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#jurusan-tab-1">
               <i class="bi bi-laptop"></i>
               <div>
                 <h4 class="d-none d-lg-block">Teknik Komputer dan Jaringan (TKJ)</h4>
-                <p>
-                  Memahami dasar-dasar jaringan komputer dan teknologi informasi.
-                </p>
+                <p>Memahami dasar-dasar jaringan komputer dan teknologi informasi.</p>
               </div>
             </a>
           </li>
@@ -28,9 +25,7 @@
               <i class="bi bi-code-slash"></i>
               <div>
                 <h4 class="d-none d-lg-block">Rekayasa Perangkat Lunak (RPL)</h4>
-                <p>
-                  Belajar tentang pengembangan perangkat lunak dan aplikasi.
-                </p>
+                <p>Belajar tentang pengembangan perangkat lunak dan aplikasi.</p>
               </div>
             </a>
           </li>
@@ -39,9 +34,7 @@
               <i class="bi bi-telephone"></i>
               <div>
                 <h4 class="d-none d-lg-block">Teknik Elektronika (Telin)</h4>
-                <p>
-                  Memahami sistem elektronika dan aplikasinya dalam kehidupan sehari-hari.
-                </p>
+                <p>Memahami sistem elektronika dan aplikasinya dalam kehidupan sehari-hari.</p>
               </div>
             </a>
           </li>
@@ -50,9 +43,7 @@
               <i class="bi bi-tools"></i>
               <div>
                 <h4 class="d-none d-lg-block">Administrasi Perkantoran (AP)</h4>
-                <p>
-                  Mempelajari pengelolaan dan administrasi di lingkungan perkantoran.
-                </p>
+                <p>Mempelajari pengelolaan dan administrasi di lingkungan perkantoran.</p>
               </div>
             </a>
           </li>
@@ -61,9 +52,7 @@
               <i class="bi bi-car-front"></i>
               <div>
                 <h4 class="d-none d-lg-block">Teknik Bisnis Sepeda Motor (TBSM)</h4>
-                <p>
-                  Pelatihan dalam perawatan dan perbaikan sepeda motor.
-                </p>
+                <p>Pelatihan dalam perawatan dan perbaikan sepeda motor.</p>
               </div>
             </a>
           </li>
@@ -72,9 +61,7 @@
               <i class="bi bi-gear"></i>
               <div>
                 <h4 class="d-none d-lg-block">Teknik Kendaraan Ringan (TKR)</h4>
-                <p>
-                  Fokus pada perawatan dan perbaikan kendaraan ringan.
-                </p>
+                <p>Fokus pada perawatan dan perbaikan kendaraan ringan.</p>
               </div>
             </a>
           </li>
@@ -83,9 +70,7 @@
               <i class="bi bi-hospital"></i>
               <div>
                 <h4 class="d-none d-lg-block">Farmasi</h4>
-                <p>
-                  Menyediakan pendidikan tentang obat-obatan dan pelayanan kesehatan.
-                </p>
+                <p>Menyediakan pendidikan tentang obat-obatan dan pelayanan kesehatan.</p>
               </div>
             </a>
           </li>
@@ -94,71 +79,41 @@
       </div>
 
       <div class="col-lg-6">
-
         <div class="tab-content" data-aos="fade-up" data-aos-delay="200">
 
           <div class="tab-pane fade active show" id="jurusan-tab-1">
-            <img src="tkj.jpg" alt="Teknik Komputer dan Jaringan" class="tkj.jpg">
+            <img src="assets/img/tkj.jpg" alt="Teknik Komputer dan Jaringan" class="img-fluid">
           </div><!-- End Tab Content Item -->
 
           <div class="tab-pane fade" id="jurusan-tab-2">
-            <img src="rpl.jpg" alt="Rekayasa Perangkat Lunak" class="img-fluid">
+            <img src="assets/img/rpl.jpg" alt="Rekayasa Perangkat Lunak" class="img-fluid">
           </div><!-- End Tab Content Item -->
 
           <div class="tab-pane fade" id="jurusan-tab-3">
-            <img src="telin.jpg" alt="Teknik Elektronika" class="img-fluid">
+            <img src="assets/img/telin.jpg" alt="Teknik Elektronika" class="img-fluid">
           </div><!-- End Tab Content Item -->
 
           <div class="tab-pane fade" id="jurusan-tab-4">
-            <img src=ap.jpg" alt="Administrasi Perkantoran" class="img-fluid">
+            <img src="assets/img/ap.jpg" alt="Administrasi Perkantoran" class="img-fluid">
           </div><!-- End Tab Content Item -->
 
           <div class="tab-pane fade" id="jurusan-tab-5">
-            <img src="tbsm.jpg" alt="Teknik Bisnis Sepeda Motor" class="img-fluid">
+            <img src="assets/img/tbsm.jpg" alt="Teknik Bisnis Sepeda Motor" class="img-fluid">
           </div><!-- End Tab Content Item -->
 
           <div class="tab-pane fade" id="jurusan-tab-6">
-            <img src="tkr.jpg" alt="Teknik Kendaraan Ringan" class="img-fluid">
+            <img src="assets/img/tkr.jpg" alt="Teknik Kendaraan Ringan" class="img-fluid">
           </div><!-- End Tab Content Item -->
 
           <div class="tab-pane fade" id="jurusan-tab-7">
-            <img src="farmasi.jpg" alt="Farmasi" class="img-fluid">
+            <img src="assets/img/farmasi.jpg" alt="Farmasi" class="img-fluid">
           </div><!-- End Tab Content Item -->
 
         </div>
-
       </div>
 
     </div>
 
   </div>
-
 </section>
 
-<!-- Features Details Section -->
-<section id="features-details" class="features-details section">
-
-  <div class="container">
-
-    <div class="row gy-4 justify-content-between features-item">
-
-      <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-        <img src="assets/img/jurusan-detail.jpg" class="img-fluid" alt="Detail Jurusan di SMKN Bantarkalong">
-      </div>
-
-      <div class="col-lg-5 d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
-        <div class="content">
-          <h3>Jurusan yang Tersedia</h3>
-          <p>
-            SMKN Bantarkalong menawarkan berbagai jurusan yang sesuai dengan kebutuhan industri dan perkembangan teknologi. Pilih jurusan yang sesuai dengan minat dan bakatmu.
-          </p>
-          <a href="#" class="btn more-btn">Pelajari lebih lanjut</a>
-        </div>
-      </div>
-
-    </div><!-- Features Item -->
-
-  </div>
-
-</section>
-<!-- /Features Details Section -->
