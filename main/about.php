b75fcac3204549d2aff2cd5a0745ed295c1536ea
ndamasfat <nndamasfat@gmail.com>
2024-10-31 11:08:03 +0700

    fixed about

--- main/about.php
+++ main/about.php
@@ -1,17 +1,13 @@
 
-<section id="about" class="about section">
+<section id="about" class="about section mt-5">
 
 <div class="container">
 
   <div class="row gy-4">
 
     <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
-      <p class="who-we-are">Siapa Kami?</p>
-      <h3>Unleashing Potential with Creative Strateg </h3>
-      <p class="fst-italic">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua.
-      </p>
+      
+      <h3>SMKN BANTARKALONG</h3>
       <ul>
         <li><i class="bi bi-check-circle"></i> <span>Visi<br>Lembaga Pendidikan dan Pelatihan yang Unggul Menghasilkan Sumber Daya  Manusia Bertaqwa,berjiwa usaha,dan Kompetitif di Nasional dan Internasional</br></span></li>
         <li><i class="bi bi-check-circle"></i> <span>Misi<br>1.Meningkatkan Pembelajaran untuk Menghasilkan Lulusan yang Beriman,Bertaqwa,Berakhlakul Karimah,Berjiwa Wirausaha dan Berbudaya Bangsa.<br>2.Mewujudkan Lembaga SMK Negeri Bantarkalong yang Berperan sebagai Pusat Pendidikan dan Pelatihan Ilmu Pengetahuan dan Teknologi serta Keterampilan untuk Meningkatkan Sumber Daya Daerah.<br>3.Mewujudkan Lembaga SMK Negeri Bantarkalong yang Berperan sebagai Pusat Layanan Masyarakat dalam Rangka Mengembangkan pembangunan Bangsa.<br>4.Memperkuat Tatakelola Melalui Penerapan SMM ISO 9001:2008 untuk Meningkatkan Pelayanan kepada Pelanggan.<br>5.Meningkatkan Fasilitas sarana dan prasarana untuk memenuhi standar pelayanan pembelajaran.<br>6.Menciptakan Lingkungan Fisik sekolah yang bersih,rapi,aman dan nyaman berwawasan Green School.<br>7.Memperluas kerja sama dengan DU/DI untuk Meningkatkan keterserapan Lulusan.<br>8.Meningkatkan prestasi sekolah dalam Bidang Akademik</span></li>
