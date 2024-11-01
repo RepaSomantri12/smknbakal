85fc1f532a283780b0342f69a0a11875ce576bf9
ndamasfat <nndamasfat@gmail.com>
2024-11-01 10:29:14 +0700

    profil

---
+++ main/profil.php
@@ -0,0 +1,53 @@
+<section id="about" class="about section mt-5">
+
+  <div class="container">
+
+    <div class="row gy-4">
+
+      <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
+        
+        <h3>TENTANG SMK NEGERI BANTARKLAONG</h3>
+        <h4>Berita Terkini</h4>
+        <ul>
+          <li><i class="bi bi-check-circle"></i> <span>Pendaftaran Siswa Baru Dibuka<br>Kami mengundang semua calon siswa untuk mendaftar dan bergabung dengan kami di SMK Negeri Bantarklaong. Daftar sekarang!</span></li>
+          <li><i class="bi bi-check-circle"></i> <span>Pelatihan Keterampilan Baru<br>Penyelenggaraan pelatihan keterampilan baru di bidang Teknologi Informasi dan Komunikasi mulai bulan depan.</span></li>
+          <li><i class="bi bi-check-circle"></i> <span>Acara Bakti Sosial<br>Kegiatan bakti sosial untuk membantu masyarakat sekitar akan dilaksanakan pada tanggal 15 November 2024.</span></li>
+        </ul>
+
+        <h4>Visi dan Misi</h4>
+        <ul>
+          <li><i class="bi bi-check-circle"></i> <span>Visi<br>Lembaga Pendidikan dan Pelatihan yang Unggul Menghasilkan Sumber Daya Manusia Bertaqwa, Berjiwa Usaha, dan Kompetitif di Nasional dan Internasional</span></li>
+          <li><i class="bi bi-check-circle"></i> <span>Misi<br>1. Meningkatkan Pembelajaran untuk Menghasilkan Lulusan yang Beriman, Bertaqwa, Berakhlakul Karimah, Berjiwa Wirausaha dan Berbudaya Bangsa.<br>2. Mewujudkan Lembaga SMK Negeri Bantarkalong yang Berperan sebagai Pusat Pendidikan dan Pelatihan Ilmu Pengetahuan dan Teknologi serta Keterampilan untuk Meningkatkan Sumber Daya Daerah.<br>3. Mewujudkan Lembaga SMK Negeri Bantarkalong yang Berperan sebagai Pusat Layanan Masyarakat dalam Rangka Mengembangkan Pembangunan Bangsa.<br>4. Memperkuat Tatakelola Melalui Penerapan SMM ISO 9001:2008 untuk Meningkatkan Pelayanan kepada Pelanggan.<br>5. Meningkatkan Fasilitas Sarana dan Prasarana untuk Memenuhi Standar Pelayanan Pembelajaran.<br>6. Menciptakan Lingkungan Fisik Sekolah yang Bersih, Rapi, Aman dan Nyaman Berwawasan Green School.<br>7. Memperluas Kerja Sama dengan DU/DI untuk Meningkatkan Keterserapan Lulusan.<br>8. Meningkatkan Prestasi Sekolah dalam Bidang Akademik.</span></li>
+        </ul>
+        
+        <a href="#" class="read-more"><span>Lihat Lebih Banyak</span><i class="bi bi-arrow-right"></i></a>
+      </div>
+
+      <div class="col-lg-6 about-images" data-aos="fade-up" data-aos-delay="200">
+        <div class="row gy-4">
+          <div class="col-lg-6">
+            <img src="images/tkr.jpg" class="img-fluid" alt="TKR">
+          </div>
+          <div class="col-lg-6">
+            <img src="images/tbsm.jpg" class="img-fluid" alt="BSM">
+          </div>
+          <div class="col-lg-6">
+            <img src="images/fkk.jpg" class="img-fluid" alt="FKK">
+          </div>
+          <div class="col-lg-6">
+            <img src="images/telka.jpg" class="img-fluid" alt="TELKA">
+          </div>
+          <div class="col-lg-6">
+            <img src="images/ap.jpg" class="img-fluid" alt="AP">
+          </div>
+          <div class="col-lg-6">
+            <img src="images/tkj.jpg" class="img-fluid" alt="TKJ">
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+</section>
